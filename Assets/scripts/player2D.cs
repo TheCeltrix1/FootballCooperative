2ee68ceefba0de0aa2f0751309cdf00bc1d9cd27
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2021-02-16 14:22:00 +0000

    camera edits

--- Assets/scripts/player2D.cs
+++ Assets/scripts/player2D.cs
@@ -6,7 +6,7 @@ public class player2D : MonoBehaviour
 {
     public float speed = 3.0f;
     public float jump = 20.0f;
-    public float energy = 0f;
+    public float energy = 3f;
     public LayerMask JumpLayer;
     public Rigidbody2D playerRigidbody;
     public Collider2D playerCollider;
@@ -23,6 +23,7 @@ public class player2D : MonoBehaviour
         if(energy <=0){       
             return;
         }
+        energy -= Time.deltaTime;
         transform.localRotation = Quaternion.Euler(0, 0, 0);
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
