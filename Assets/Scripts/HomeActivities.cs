b3a5722d3403dfb38814b47d6f876c965064e929
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2021-02-27 00:39:03 +0000

    Activities V1

---
+++ Assets/Scripts/HomeActivities.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace HomeCode
+{
+    public class HomeActivities : MonoBehaviour
+    {
+        public AudioClip[] audioClips;
+        public GameObject buttonHolder;
+        public float timerLength;
+        private float _timer;
+        private bool _moveButtonHolder;
+
+        private Vector2 _altPos;
+        private Vector2 _originalPos;
+        private AudioSource _audioSource;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            if (GetComponent<AudioSource>() == false) gameObject.AddComponent<AudioSource>();
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.playOnAwake = false;
+            _audioSource.volume = 0.25f;
+            _altPos = new Vector2(buttonHolder.transform.position.x, -buttonHolder.GetComponentInParent<Canvas>().GetComponent<RectTransform>().rect.height);
+            _originalPos = buttonHolder.transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown("k")) ResetActivities();
+            if (_moveButtonHolder && _timer < timerLength)
+            {
+                _timer += Time.deltaTime;
+                buttonHolder.transform.position = Vector2.Lerp(_altPos, _originalPos, _timer / timerLength);
+            }
+            else _moveButtonHolder = false;
+        }
+
+        public void ResetActivities()
+        {
+            buttonHolder.transform.position = _altPos;
+            buttonHolder.SetActive(true);
+            _timer = 0;
+            _moveButtonHolder = true;
+        }
+
+        public void Click(int i)
+        {
+            ChooseActivity(i, buttonHolder);
+        }
+
+        void ChooseActivity(int i, GameObject obj = default(GameObject))
+        {
+            if (audioClips[i] != null)
+            {
+                _audioSource.clip = audioClips[i];
+                _audioSource.Play();
+                IEnumerator rou = FadInLoading.LoadTransition(audioClips[i].length, obj);
+                StartCoroutine(rou);
+            }
+            else Debug.Log($"{i} is out of bounds");
+        }
+    }
+}
