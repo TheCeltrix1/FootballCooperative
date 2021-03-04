51d780e38389a93e20265265b6c8e4b7b0c6813e
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-03-04 14:59:05 +0000

    home art, added to canvas

--- Assets/scripts/souvenirs.cs
+++ Assets/scripts/souvenirs.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+    
 public class souvenirs : MonoBehaviour
 {
     public float threshold;
     private GameManager manager;
-    private SpriteRenderer spirte;
+    private Image spirte;
     // Start is called before the first frame update
     void Start()
     {
         manager = GameManager.instance;
-        this.spirte = GetComponent<SpriteRenderer>();
+        this.spirte = GetComponent<Image>();
         if (threshold <= manager.trips)
         {
             this.spirte.enabled = true;
