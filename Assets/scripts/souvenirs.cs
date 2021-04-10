210213e838141654a9db7c7d5ce276c1606bb6bb
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2021-04-10 19:29:35 +0100

    souvenir's put into home scene

--- Assets/scripts/souvenirs.cs
+++ Assets/scripts/souvenirs.cs
@@ -8,11 +8,13 @@ public class souvenirs : MonoBehaviour
     public float threshold;
     private GameManager manager;
     private Image spirte;
+   // private SpriteRenderer spirte;
     // Start is called before the first frame update
     void Start()
     {
         manager = GameManager.instance;
         this.spirte = GetComponent<Image>();
+      //  this.spirte = GetComponent<SpriteRenderer>();
         if (threshold <= manager.trips)
         {
             this.spirte.enabled = true;
