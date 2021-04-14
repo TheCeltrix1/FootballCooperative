bff8a8cb7b9dc38dadf7f8b2935c80061584aebe
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2021-04-14 22:03:06 +0100

    Final Push for Audio tonight

--- Assets/scripts/Player_walking.cs
+++ Assets/scripts/Player_walking.cs
@@ -7,6 +7,7 @@ public class Player_walking : MonoBehaviour
     public float movementspeed = 1;
     public bool facingRight = true;
     public Canvas canvieBoi;
+    public AudioSource walkSFX;
 
     private float _pos;
     private float _spriteSize;
@@ -33,8 +34,16 @@ public class Player_walking : MonoBehaviour
     private void Movement()
     {
         if (Input.GetMouseButtonDown(0)) _pos = MousePositionEqualHeight();
-        if (Vector2.Distance(new Vector2(_pos, 0), new Vector2(transform.localPosition.x, 0)) > 1f && movementspeed != 0) _playerAnimator.SetFloat("speed", 0.8f);
-        else _playerAnimator.SetFloat("speed", 0);
+        if (Vector2.Distance(new Vector2(_pos, 0), new Vector2(transform.localPosition.x, 0)) > 1f && movementspeed != 0)
+        {
+            _playerAnimator.SetFloat("speed", 0.8f);
+            if (!walkSFX.isPlaying) walkSFX.Play();
+        }
+        else
+        {
+            _playerAnimator.SetFloat("speed", 0);
+            walkSFX.Stop();
+        }
         if (_pos < transform.localPosition.x) _spriteRenderer.flipX = true;
         else _spriteRenderer.flipX = false;
         transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(_pos, transform.localPosition.y, transform.localPosition.z), movementspeed * Time.deltaTime);
