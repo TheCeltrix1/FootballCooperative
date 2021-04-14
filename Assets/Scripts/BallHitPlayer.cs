eed4052656cc29df33b231503a06ebb56da8d62c
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2021-04-14 14:36:24 +0100

    Hit will Ball

---
+++ Assets/Scripts/BallHitPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHitPlayer : MonoBehaviour
+{
+    public float hitTime = 2f;
+    [HideInInspector] public Transform playerFacePosition;
+    private float _hitTimer = 0;
+    public IEnumerator HitPlayerInTheFace()
+    {
+        while (_hitTimer < hitTime)
+        {
+            transform.position = Vector3.Lerp(transform.position, playerFacePosition.position, _hitTimer/hitTime);
+            _hitTimer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
