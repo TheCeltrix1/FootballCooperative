58032216d58f55595871817035ab1aed7e86cc04
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2021-04-18 18:26:28 +0100

    All Done

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -14,6 +14,7 @@ public class PauseGame : MonoBehaviour
     public TextMeshProUGUI characterText;
     public TextMeshProUGUI choresText;
     public TextMeshProUGUI coOpText;
+    public GameObject darkness;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +25,7 @@ public class PauseGame : MonoBehaviour
             coOpText.enabled = false;
             paused = true;
             _tutorialPlayed = true;
-
+            darkness.SetActive(true);
         }
         else
         {
@@ -32,9 +33,10 @@ public class PauseGame : MonoBehaviour
             characterText.enabled = false;
             choresText.enabled = false;
             coOpText.enabled = false;
+            darkness.SetActive(false);
         }
-            
-           
+
+
         textCount = 2f;
     }
 
@@ -94,5 +96,6 @@ public class PauseGame : MonoBehaviour
     public void ResumeGameTime()
     {
         Time.timeScale = 1;
+        darkness.SetActive(false);
     }
 }
